eda4e898e3aa84c441c9324233c71e5e23e601ed
priyaprabhu936 <asalappu143@gmail.com>
2025-08-24 10:11:15 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+// Backend base URL
 const API = axios.create({
-  baseURL: "https://ott-backend-imh7.onrender.com/api",  // un backend URL
+  baseURL: "https://ott-backend-imh7.onrender.com/api",
 });
 
+// Movies
 export const getMovies = () => API.get("/movies");
 export const addMovie = (movieData) => API.post("/movies", movieData);
+export const updateMovie = (id, movieData) => API.put(`/movies/${id}`, movieData);
 export const deleteMovie = (id) => API.delete(`/movies/${id}`);
 
 export default API;
