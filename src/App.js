7fd223f43af65781138dbc14cf2169f83ac9b3fa
priyaprabhu936 <asalappu143@gmail.com>
2025-08-25 15:57:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import MovieList from "./components/MovieList";
 import AddMovie from "./components/AddMovie";
 import Register from "./components/Register";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
-        <h1>OTT Platform</h1>
-        <Routes>
-          <Route path="/" element={<MovieList />} />
-          <Route path="/add" element={<AddMovie />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+      <div className="app">
+        {/* Top bar like Netflix */}
+        <header className="topbar">
+          <div className="brand">CineStream</div>
+          <nav className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/add">Add</Link>
+            <Link to="/register">Register</Link>
+          </nav>
+        </header>
+
+        {/* Page content */}
+        <main className="page">
+          <Routes>
+            <Route path="/" element={<MovieList />} />
+            <Route path="/add" element={<AddMovie />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+
+        <footer className="footer">© {new Date().getFullYear()} CineStream</footer>
       </div>
     </Router>
   );
