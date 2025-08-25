9c6f95bfc963d52d33c2c04867e934b2c72c10a8
priyaprabhu936 <asalappu143@gmail.com>
2025-08-25 18:41:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieList from "./components/MovieList";
-import AddMovie from "./components/AddMovie";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MovieGrid from "./components/MovieGrid";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MovieList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/add"
-          element={
-            <ProtectedRoute>
-              <AddMovie />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <div className="app">
+        {/* Navbar */}
+        <nav className="navbar">
+          <h2 className="logo">OTT Platform</h2>
+          <div className="nav-links">
+            <Link to="/">Movies</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<MovieGrid />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
+
 export default App;
